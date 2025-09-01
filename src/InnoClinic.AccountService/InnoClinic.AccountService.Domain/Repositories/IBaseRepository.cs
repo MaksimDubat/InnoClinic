11765d6b3f2fe8dc2cf2bc743329bf7ab1973bbb
dubatovka.maksim <dubatovvka.maksim@gmail.com>
2025-09-01 16:27:24 +0300

    feature: US-0.1 / Added base repository

---
+++ src/InnoClinic.AccountService/InnoClinic.AccountService.Domain/Repositories/IBaseRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq.Expressions;
+
+namespace InnoClinic.AccountService.Application.Interfaces;
+
+public interface IBaseRepository<T> where T : class
+{
+    Task<List<T>> GetAllAsync(Func<IQueryable<T>, IQueryable<T>> queryBuilder, CancellationToken cancellation);
+    Task<T> GetAsync(int id, CancellationToken cancellation);
+    Task AddAsync(T entity, CancellationToken cancellation);
+    void Update(T entity);
+    Task<T> DeleteAsync(int id, CancellationToken cancellation);
+    Task<bool> AnyAsync(Expression<Func<T, bool>> predicate, CancellationToken cancellation);
+}
