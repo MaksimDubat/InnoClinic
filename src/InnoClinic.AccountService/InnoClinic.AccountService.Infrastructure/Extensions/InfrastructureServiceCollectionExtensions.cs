0981074c8b50d61eadd762e57e38d54754bb7be5
dubatovka.maksim <dubatovvka.maksim@gmail.com>
2025-09-01 16:18:56 +0300

    refactor: US-0 / apply PR feedback

---
+++ src/InnoClinic.AccountService/InnoClinic.AccountService.Infrastructure/Extensions/InfrastructureServiceCollectionExtensions.cs
@@ -0,0 +1,25 @@
+﻿using InnoClinic.AccountService.Application.Interfaces;
+using InnoClinic.AccountService.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace InnoClinic.AccountService.Infrastructure.Registrations;
+
+public static class InfrastructureServiceCollectionExtensions
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        string? connectionString = configuration.GetConnectionString("AccountDb");
+
+        services.AddDbContext<AccountServiceDbContext>(options =>
+           options.UseNpgsql(connectionString,
+               b => b.MigrationsAssembly(typeof(AccountServiceDbContext).Assembly.GetName().Name)
+           )
+        );
+
+        services.AddScoped(typeof(IBaseRepository<>), typeof(BaseRepository<>));
+
+        return services;
+    }
+}
--- src/InnoClinic.AccountService/InnoClinic.AccountService.WebAPI/Program.cs
+++ src/InnoClinic.AccountService/InnoClinic.AccountService.WebAPI/Program.cs
@@ -6,7 +6,7 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        var builder = WebApplication.CreateBuilder(args);
+        WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
         // Add services to the container.
         builder.Services.AddApplicationRegistrations(builder.Configuration);
@@ -16,7 +16,7 @@ public class Program
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
 
-        var app = builder.Build();
+        WebApplication app = builder.Build();
 
         // Configure the HTTP request pipeline.
         if (app.Environment.IsDevelopment())
